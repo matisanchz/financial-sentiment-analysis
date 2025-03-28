741627313c2f81dba886d6cf7efd83b701e275a7
Matias <matiassanchezabrego@gmail.com>
2025-03-28 20:16:27 -0300

    Add popup validator

--- src/app.py
+++ src/app.py
@@ -6,9 +6,8 @@ from utils import generate_date_list
 from PIL import Image
 import os
 
-
 def get_sentiment_analysis(app, frame, date_from, date_to, keyword):
-    if keyword != "":
+    if keyword != "" and date_from <= date_to:
         print(keyword)
 
         model = FinbertModel()
@@ -49,6 +48,28 @@ def get_sentiment_analysis(app, frame, date_from, date_to, keyword):
         return_app = CTkButton(master=app, text="Return", command=lambda: return_main(app))
         return_app.pack()
 
+    else:
+        open_popup(keyword, date_from, date_to)
+
+def open_popup(keyword, date_from, date_to):
+    popup = CTkToplevel()
+    popup.title("Popup")
+
+    if keyword == "":
+
+        label1 = CTkLabel(popup, text="You must enter a valid keyword", font=("Arial", 14))
+        label1.pack(pady=20, padx=20)
+
+    if date_from > date_to:
+        label2 = CTkLabel(popup, text="Date 'To' must be after Date 'From'.", font=("Arial", 14))
+        label2.pack(pady=20, padx=20)
+
+    close_button = CTkButton(popup, text="Cerrar", command=popup.destroy)
+    close_button.pack(pady=10)
+
+    popup.transient()
+    popup.grab_set()
+
 def return_main(app):
     app.destroy()
     main()
@@ -60,6 +81,7 @@ def main():
 
     app = CTk()
     app.geometry("600x500")
+    app.title("Financial Sentiment Analysis")
 
     body = CTkFrame(app)
     body.pack(pady=20, padx=60, fill="both", expand=True)
@@ -93,16 +115,31 @@ def main():
 
     dates=generate_date_list()
 
-    from_box = CTkComboBox(master=body, 
+    from_frame = CTkFrame(body, fg_color="transparent")
+    from_frame.pack(pady=5, padx=10)
+    
+    from_label = CTkLabel(from_frame, text="From:")
+    from_label.pack(side="left", padx=5)
+
+    from_box = CTkComboBox(from_frame, 
                            values=dates, 
                            corner_radius=10)
     
     from_box.pack(pady=12,padx=10)
 
-    to_box = CTkComboBox(master=body, values=dates, corner_radius=10)
+    to_frame = CTkFrame(body, fg_color="transparent")
+    to_frame.pack(pady=5, padx=10)
+    
+    to_label = CTkLabel(to_frame, text="To:")
+    to_label.pack(side="left", padx=5)
+
+    to_box = CTkComboBox(to_frame, 
+                        values=dates, 
+                        corner_radius=10)
+    
     to_box.pack(pady=12,padx=10)
 
-    input = CTkEntry(master=body)
+    input = CTkEntry(master=body, placeholder_text="Enter the keyword here.")
     input.pack(pady=12,padx=10)
 
     btn = CTkButton(master=body, 
